8ae1caa37128b56b840e9c79b6e5e6732a5f9415
brian <brian@ten9.com>
2020-09-23 11:24:22 -0700

    Reworking project from scratch

--- src/main.js
+++ src/main.js
@@ -1,28 +1,12 @@
-import 'element-ui/lib/theme-chalk/index.css'
-import '@/style/mxgraph.css'
-import '@/style/style.scss'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-
-import * as R from 'ramda'
-import ElementUI from 'element-ui'
-import routes from './router/index.js';
-
-Vue.use(ElementUI)
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-})
-
-Vue.prototype.R = R
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  render: h => h(App)
+}).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,3 @@
 module.exports = {
-    publicPath: '/',
-    outputDir: 'dist',
-    lintOnSave: true,
-    configureWebpack: {
-      devtool: 'source-map'
-    },
-    chainWebpack: (config) => {
-      config.module
-        .rule('')
-        .test(/mxClient\.js$/)
-        .use('exports-loader')
-        .loader('exports-loader?mxClient,mxToolbar,mxEdgeStyle,mxConnectionHandler,mxEllipse,mxConnectionConstraint,mxWindow,' +
-          'mxObjectCodec,mxGraphModel,mxActor,mxPopupMenu,mxShape,mxEventObject,mxGraph,mxPopupMenuHandler,mxPrintPreview,' +
-          'mxEventSource,mxRectangle,mxVertexHandler,mxMouseEvent,mxGraphView,mxCodecRegistry,mxImage,mxGeometry,mxCellState,' +
-          'mxRubberband,mxConstraintHandler,mxKeyHandler,mxDragSource,mxGraphModel,mxEvent,mxUtils,mxEvent,mxCodec,mxCell,' +
-          'mxConstants,mxPoint,mxGraphHandler,mxCylinder,mxCellRenderer,mxEvent,mxUndoManager,mxStencilRegistry,mxStencil')
-        .end()
-    }
-  }
+  lintOnSave: false
+};
