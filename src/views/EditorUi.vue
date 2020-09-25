b3078a61d6ea438f4c3660dad5846183b94ab1a5
brian <brian@ten9.com>
2020-09-24 19:22:55 -0700

    Using 1 component to wrap all of GraphEditor
    There's still a bug with the geDiagramContainer class not having any height.\nAlso the sidebars, toolbars, etc. are all disabled

--- src/views/EditorUi.vue
+++ src/views/EditorUi.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="editor-ui">
-    <editor />
-  </div>
+  <div class="editor-ui" ref="container" />
 </template>
 
 <script>
-import Editor from '@/components/Editor.vue';
+import EditorUi from '@/lib/jgraph/EditorUi.js';
 
 export default {
   name: 'EditorUi',
-  components: {
-    Editor,
+  data() {
+    return {
+      editorUi: null,
+      editor: null,
+      graph: null,
+    };
+  },
+  mounted() {
+    this.editorUi = new EditorUi(null, this.$refs.container);
+
+    this.graph = this.editorUi.editor.graph;
+
+    // Gets the default parent for inserting new cells. This
+    // is normally the first child of the root (ie. layer 0).
+    var parent = this.graph.getDefaultParent();
+
+    // Adds cells to the model in a single step
+    this.graph.getModel().beginUpdate();
+    try {
+      let v1 = this.graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
+      let v2 = this.graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
+
+      this.graph.insertEdge(parent, null, '', v1, v2);
+    } finally {
+      // Updates the display
+      this.graph.getModel().endUpdate();
+    }
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,10 @@ module.exports = {
           'mxObjectCodec,mxGraphModel,mxActor,mxPopupMenu,mxShape,mxEventObject,mxGraph,mxPopupMenuHandler,mxPrintPreview,' +
           'mxEventSource,mxRectangle,mxVertexHandler,mxMouseEvent,mxGraphView,mxCodecRegistry,mxImage,mxGeometry,mxCellState,' +
           'mxRubberband,mxConstraintHandler,mxKeyHandler,mxDragSource,mxGraphModel,mxEvent,mxUtils,mxEvent,mxCodec,mxCell,' +
-          'mxConstants,mxPoint,mxGraphHandler,mxCylinder,mxCellRenderer,mxEvent,mxUndoManager,mxStencilRegistry,mxStencil',
+          'mxConstants,mxPoint,mxGraphHandler,mxCylinder,mxCellRenderer,mxEvent,mxUndoManager,mxStencilRegistry,mxStencil,' +
+          'mxText,mxSvgCanvas2D,mxCellHighlight,mxStackLayout,mxConnector,mxEdgeHandler,mxGuide,mxCellEditor,' +
+          'mxSelectionCellsHandler,mxOutline,mxPanningHandler,mxResources,mxLayoutManager,mxRectangleShape,mxDictionary,' +
+          'mxClipboard,mxGeometryChange,mxValueChange',
       )
       .end();
   },
