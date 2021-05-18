0afc7a211a4ff9b9f9bdb39dab13e1b474828b15
brian <brian@ten9.com>
2021-05-18 09:54:22 -0700

    Adding ten9 headers

--- src/graph_editor/components/vue-colorpicker/color/Preview.vue
+++ src/graph_editor/components/vue-colorpicker/color/Preview.vue
@@ -1,3 +1,19 @@
+<!--
+* ten9, Inc
+* Copyright (c) 2015 - 2021 ten9, Inc
+* -----
+* NOTICE:  All information contained herein is, and remains
+* the property of ten9 Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to ten9 Incorporated
+* and its suppliers and may be covered by U.S. and Foreign Patents,
+* patents in process, and are protected by trade secret or copyright law.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from ten9 Incorporated.
+* -----
+-->
+
 <template>
   <!-- // TEN9: Setting to 100 explicitly for best UX -->
   <canvas style="max-width: 100%;"/>
