89d0aeb6f9f89b806f20a637ad59388071a4faa4
brian <brian@ten9.com>
2021-08-17 20:42:30 -0700

    More fixes

--- src/apps/graph_editor/components/menu/PopupMenu.vue
+++ src/apps/graph_editor/components/menu/PopupMenu.vue
@@ -24,7 +24,7 @@ import {
   mxUtils,
 } from '../../lib/jgraph/mxClient.js';
 import { Editor } from '../../lib/jgraph/Editor.js';
-import { defineComponent, onMounted, onUnmounted, ref, watch } from '@vue/composition-api';
+import { defineComponent, nextTick, onMounted, onUnmounted, ref } from '@vue/composition-api';
 import VClamp from 'vue-clamp';
 const graphUtils = require('../../lib/jgraph/graph_utils.js');
 
@@ -52,6 +52,8 @@ class EnabledState {
   clearDefaultStyle: boolean;
   toFront: boolean;
   toBack: boolean;
+  bringForward: boolean;
+  sendBackward: boolean;
   line: boolean;
   reverse: boolean;
   addWaypoint: boolean;
@@ -82,32 +84,31 @@ export default defineComponent({
 
     const show = ref<boolean>(false);
 
-    const cellSelectedVisible = ref<boolean>(false);
-
     const controlKey = ref<string>(Editor.ctrlKey);
 
     const left = ref<number>(0);
 
     const top = ref<number>(0);
 
-    const isMultipleCellSelected = ref<boolean>(false);
-
     const { graph } = props.editorUi.editor;
 
     const enabledState = ref<EnabledState>(new EnabledState());
+
+    function isEnabled(key: string) {
+      return enabledState.value[key];
+    }
 
     function updateState() {
       const cell = graph.getSelectionCell();
       const state = graph.view.getState(cell);
       const selectionCount = graph.getSelectionCount();
       const coordinates = graphUtils.getDocumentContainerRect();
+      let hasWaypoints = false;
 
       left.value = graph.lastMouseX - coordinates.x;
       top.value = graph.lastMouseY - coordinates.y;
 
       isSelectionEmpty.value = selectionCount === 0;
-      isMultipleCellSelected.value = selectionCount > 1;
-      cellSelectedVisible.value = selectionCount > 0;
 
       enabledState.value.undo =
         isSelectionEmpty.value && props.editorUi.actions.get('undo').isEnabled();
@@ -123,6 +124,8 @@ export default defineComponent({
       enabledState.value.clearDefaultStyle = selectionCount !== 1 && isSelectionEmpty.value;
       enabledState.value.toFront = !isSelectionEmpty.value;
       enabledState.value.toBack = !isSelectionEmpty.value;
+      enabledState.value.bringForward = !isSelectionEmpty.value;
+      enabledState.value.sendBackward = !isSelectionEmpty.value;
 
       enabledState.value.group = selectionCount > 1;
       enabledState.value.ungroup =
@@ -136,7 +139,8 @@ export default defineComponent({
       if (
         graph.getModel().isEdge(cell) &&
         mxUtils.getValue(state.style, mxConstants.STYLE_EDGE, null) != 'entityRelationEdgeStyle' &&
-        mxUtils.getValue(state.style, mxConstants.STYLE_SHAPE, null) != 'arrow') {
+        mxUtils.getValue(state.style, mxConstants.STYLE_SHAPE, null) != 'arrow'
+      ) {
         enabledState.value.reverse = true;
 
         let isWaypoint = false;
@@ -156,52 +160,44 @@ export default defineComponent({
 
         enabledState.value.addWaypoint = !isWaypoint;
         enabledState.value.removeWaypoint = isWaypoint;
+
+        // Adds reset waypoints option if waypoints exist
+        const geo = graph.getModel().getGeometry(cell);
+        hasWaypoints = geo != null && geo.points != null && geo.points.length > 0;
       } else {
         enabledState.value.reverse = false;
         enabledState.value.addWaypoint = false;
         enabledState.value.removeWaypoint = false;
       }
 
-<<<<<<< Updated upstream
-      enabledState.value.reverse =
-        graph.getModel().isEdge(cell) &&
-        mxUtils.getValue(state.style, mxConstants.STYLE_EDGE, null) != 'entityRelationEdgeStyle' &&
-        mxUtils.getValue(state.style, mxConstants.STYLE_SHAPE, null) != 'arrow';
-
-      reverse: boolean;
-      addWaypoint: boolean;
-      removeWaypoint: boolean;
-      clearWaypoints: boolean;
-=======
-      clearWaypoints: boolean;
+      enabledState.value.clearWaypoints =
+        selectionCount === 1 &&
+        (hasWaypoints ||
+          (graph.getModel().isVertex(cell) && graph.getModel().getEdgeCount(cell) > 0));
 
       enabledState.value.editStyle = selectionCount === 1;
-      enabledState.value.editdata = selectionCount === 1;
-      enabledState.value.editStyle = selectionCount === 1;
+      enabledState.value.editData = selectionCount === 1;
+      enabledState.value.editLink = selectionCount === 1;
 
->>>>>>> Stashed changes
-      editStyle: boolean;
-      editData: boolean;
-      editLink: boolean;
+      enabledState.value.selectVertices = selectionCount === 0;
+      enabledState.value.selectEdges = selectionCount === 0;
+      enabledState.value.selectAll = selectionCount === 0;
 
       enabledState.value.editImage =
         selectionCount === 1 &&
         graph.getModel().isVertex(cell) &&
         mxUtils.getValue(state.style, mxConstants.STYLE_IMAGE, null) !== null;
-
-      selectVertices: boolean;
-      selectEdges: boolean;
-      selectAll: boolean;
     }
 
     function openPopupMenu() {
-      updateState();
+      show.value = true;
+      nextTick(() => {
+        updateState();
+      });
     }
 
     function close() {
-      isSelectionEmpty.value = true;
-      cellSelectedVisible.value = false;
-      isMultipleCellSelected.value = false;
+      show.value = false;
     }
 
     onMounted(() => {
@@ -268,22 +264,20 @@ export default defineComponent({
     // );
 
     return {
-      cellSelectedVisible,
       close,
       controlKey,
       doAction,
-      enabledState,
+      editImage,
       graph,
       hide,
+      isEnabled,
       isSelectionEmpty,
-      isMultipleCellSelected,
       left,
       mxClient,
       openPopupMenu,
       show,
       showSubmenu,
       top,
-      undoEnabled,
     };
   },
 });
@@ -295,141 +289,210 @@ div
     v-show='show',
     v-bind:style='{ left: left + "px", top: top + "px" }'
   )
-    b-list-group-item.none-border(@click='doAction("undo")', v-show='undoEnabled')
+    //- Popup Menu History Items
+    b-list-group-item.none-border(@click='doAction("undo")', v-show='isEnabled("undo")')
       span.material-icons.menu-icons undo
       span.item-name Undo
       span.shortcut {{ controlKey }}{{ !mxClient.IS_MAC ? "+" : "" }}Z
-    b-list-group-item.none-border(@click='doAction("redo")', v-show='redoEnabled')
+    b-list-group-item.none-border(@click='doAction("redo")', v-show='isEnabled("redo")')
       span.material-icons.menu-icons redo
       span.item-name Redo
       span.shortcut {{ controlKey }}+Shift+Z
-    b-list-group-item.none-border(@click='doAction("pasteHere")')
+
+    //- Popup Menu Edit Items
+    b-list-group-item.none-border(@click='doAction("pasteHere")', v-show='isEnabled("pasteHere")')
       span.material-icons.menu-icons content_paste
       span.item-name Paste Here
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal')
-    b-list-group-item.none-border(@click='doAction("clearDefaultStyle")')
-      span.item-name Clear Default Style
-      span.shortcut {{ controlKey }}+Shift+R
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal')
-    b-list-group-item.none-border(@click='doAction("selectVertices")')
-      span.item-name Select Vertices
-      span.shortcut {{ controlKey }}+Shift+I
-    b-list-group-item.none-border(@click='doAction("selectEdges")')
-      span.item-name Select Edges
-      span.shortcut {{ controlKey }}+Shift+E
-    b-list-group-item.none-border(@click='doAction("selectAll")')
-      span.item-name Select All
-      span.shortcut {{ controlKey }}{{ !mxClient.IS_MAC ? "+" : "" }}A
-  b-list-group.w-22.position-absolute.cursor-pointer(
-    v-show='somethingSelected',
-    v-bind:style='{ left: left + "px", top: top + "px" }'
-  )
-    b-list-group-item.none-border(@click='doAction("delete")')
+    b-list-group-item.none-border(@click='doAction("delete")', v-show='isEnabled("delete")')
       span.item-name Delete
       span.shortcut Delete
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal')
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("cut")'
+    )
     b-list-group-item.none-border(@click='doAction("cut")')
       span.material-icons.menu-icons content_cut
       span.item-name Cut
       span.shortcut {{ controlKey }}{{ !mxClient.IS_MAC ? "+" : "" }}X
-    b-list-group-item.none-border(@click='doAction("pasteHere")')
+    b-list-group-item.none-border(@click='doAction("copy")')
       span.material-icons.menu-icons content_copy
       span.item-name Copy
       span.shortcut {{ controlKey }}{{ !mxClient.IS_MAC ? "+" : "" }}C
-    b-list-group-item.none-border(@click='doAction("clearDefaultStyle")')
-      span.item-name Copy as Image
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal')
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("duplicate")'
+    )
     b-list-group-item.none-border(@click='doAction("duplicate")')
       span.item-name Duplicate
       span.shortcut {{ controlKey }}{{ !mxClient.IS_MAC ? "+" : "" }}D
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal')
+
+    //- Popup Menu Style Items
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("setAsDefaultStyle")'
+    )
     b-list-group-item.none-border(
       @click='doAction("setAsDefaultStyle")',
-      v-show='!isMultipleCellSelected'
+      v-show='isEnabled("setAsDefaultStyle")'
     )
       span.item-name Set as Default Style
       span.shortcut {{ controlKey }}+Shift+D
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal')
-    b-list-group-item.none-border(@click='doAction("toFront")')
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("clearDefaultStyle")'
+    )
+    b-list-group-item.none-border(
+      @click='doAction("clearDefaultStyle")',
+      v-show='isEnabled("clearDefaultStyle")'
+    )
+      span.item-name Clear Default Style
+      span.shortcut {{ controlKey }}+Shift+R
+
+    //- Popup Menu Arrange Items
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("toFront")'
+    )
+    b-list-group-item.none-border(@click='doAction("toFront")', v-show='isEnabled("toFront")')
       span.item-name To Front
       span.shortcut {{ controlKey }}+Shift+F
-    b-list-group-item.none-border(@click='doAction("toBack")')
+    b-list-group-item.none-border(@click='doAction("toBack")', v-show='isEnabled("toBack")')
       span.item-name To Back
       span.shortcut {{ controlKey }}+Shift+B
-    b-list-group-item.none-border(@click='doAction("bringForward")')
+    b-list-group-item.none-border(
+      @click='doAction("bringForward")',
+      v-show='isEnabled("bringForward")'
+    )
       span.item-name Bring Forward
-    b-list-group-item.none-border(@click='doAction("sendBackward")')
+    b-list-group-item.none-border(
+      @click='doAction("sendBackward")',
+      v-show='isEnabled("sendBackward")'
+    )
       span.item-name Send Backward
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal')
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("group") || isEnabled("ungroup")'
+    )
+    b-list-group-item.none-border(@click='doAction("group")', v-show='isEnabled("group")')
+      span.item-name Group
+      span.shortcut {{ controlKey }}+G
+    b-list-group-item(@click='doAction("ungroup")', v-show='isEnabled("ungroup")')
+      span.item-name Ungroup
+      span.shortcut {{ controlKey }}+Shift+G
+
+    //- Popup Menu Cell Items
+    //- hr.popup-dropdown-divider(
+    //-   role='separator',
+    //-   aria-orientation='horizontal',
+    //-   v-show='isEnabled("line")'
+    //- )
     //- b-dropdown#line-dropright.sub-menu(
     //-   dropright='',
     //-   text='Line',
     //-   block,
     //-   @mouseover.native='showSubmenu("line-dropright")',
     //-   @mouseleave.native='hide("line-dropright")',
-    //-   v-show='isLine'
+    //-   v-show='isEnabled("line")'
     //- )
-    //-   b-dropdown-item(href='#', @click='doAction("insertRectangle")')
-    //-     span.material-icons open_in_full
-    //-   b-dropdown-item(href='#', @click='doAction("insertEllipse")')
-    //-     .geIcon.geSprite.geSprite-orthogonal
-    //-   b-dropdown-item(href='#', @click='doAction("insertRhombus")')
-    //-     .geIcon.geSprite.geSprite-horizontalelbow
-    //-   b-dropdown-item(href='#', @click='doAction("insertText")')
-    //-     .geIcon.geSprite.geSprite-verticalelbow
-    //-   b-dropdown-item(href='#', @click='doAction("link")')
-    //-     .geIcon.geSprite.geSprite-horizontalisometric
-    //-   b-dropdown-item(href='#', @click='doAction("image")')
-    //-     .geIcon.geSprite.geSprite-verticalisometric
-    //-   b-dropdown-item(href='#', @click='doAction("image")')
-    //-     .geIcon.geSprite.geSprite-curved
-    //-   b-dropdown-item(href='#', @click='doAction("image")')
-    //-     .geIcon.geSprite.geSprite-entity
-    //-   b-dropdown-item(href='#', @click='doAction("insertRectangle")')
-    //-     span.material-icons east
-    //-   b-dropdown-item(href='#', @click='doAction("insertRectangle")')
-    //-     .geIcon.geSprite.geSprite-linkedge
-    //-   b-dropdown-item(href='#', @click='doAction("insertRectangle")')
-    //-     .geIcon.geSprite.geSprite-arrow
-    //-   b-dropdown-item(href='#', @click='doAction("insertRectangle")')
-    //-     .geIcon.geSprite.geSprite-simplearrow
-    //- hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal', v-show='isLine')
-    b-list-group-item.none-border(@click='doAction("turn")', v-show='isLine')
+    //- TODO: check these
+      b-dropdown-item(href='#', @click='doAction("insertRectangle")')
+        span.material-icons open_in_full
+      b-dropdown-item(href='#', @click='doAction("insertEllipse")')
+        .geIcon.geSprite.geSprite-orthogonal
+      b-dropdown-item(href='#', @click='doAction("insertRhombus")')
+        .geIcon.geSprite.geSprite-horizontalelbow
+      b-dropdown-item(href='#', @click='doAction("insertText")')
+        .geIcon.geSprite.geSprite-verticalelbow
+      b-dropdown-item(href='#', @click='doAction("link")')
+        .geIcon.geSprite.geSprite-horizontalisometric
+      b-dropdown-item(href='#', @click='doAction("image")')
+        .geIcon.geSprite.geSprite-verticalisometric
+      b-dropdown-item(href='#', @click='doAction("image")')
+        .geIcon.geSprite.geSprite-curved
+      b-dropdown-item(href='#', @click='doAction("image")')
+        .geIcon.geSprite.geSprite-entity
+      b-dropdown-item(href='#', @click='doAction("insertRectangle")')
+        span.material-icons east
+      b-dropdown-item(href='#', @click='doAction("insertRectangle")')
+        .geIcon.geSprite.geSprite-linkedge
+      b-dropdown-item(href='#', @click='doAction("insertRectangle")')
+        .geIcon.geSprite.geSprite-arrow
+      b-dropdown-item(href='#', @click='doAction("insertRectangle")')
+        .geIcon.geSprite.geSprite-simplearrow
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("reverse")'
+    )
+    b-list-group-item.none-border(@click='doAction("turn")', v-show='isEnabled("reverse")')
       span.item-name Reverse
       span.shortcut {{ controlKey }}+R
-    b-list-group-item.none-border(@click='doAction("addWaypoint")', v-show='isLine')
-      span.item-name Add Waypoints
-    b-list-group-item.none-border(@click='doAction("removeWaypoint")', v-show='isLine')
-      span.item-name Remove Waypoints
-    b-list-group-item.none-border(@click='doAction("clearWaypoints")', v-show='isLine')
+    b-list-group-item.none-border(
+      @click='doAction("addWaypoint")',
+      v-show='isEnabled("addWaypoint")'
+    )
+      span.item-name Add Waypoint
+    b-list-group-item.none-border(
+      @click='doAction("removeWaypoint")',
+      v-show='isEnabled("removeWaypoint")'
+    )
+      span.item-name Remove Waypoint
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("clearWaypoints")'
+    )
+    b-list-group-item.none-border(
+      @click='doAction("clearWaypoints")',
+      v-show='isEnabled("clearWaypoints")'
+    )
       span.item-name Clear Waypoints
       span.shortcut Alt+Shift+C
-    hr.popup-dropdown-divider(role='separator', aria-orientation='horizontal', v-show='isLine')
-    b-list-group-item.none-border(@click='doAction("editStyle")', v-show='!isMultipleCellSelected')
+    hr.popup-dropdown-divider(
+      role='separator',
+      aria-orientation='horizontal',
+      v-show='isEnabled("editStyle")'
+    )
+    b-list-group-item.none-border(@click='doAction("editStyle")', v-show='isEnabled("editStyle")')
       span.item-name Edit Style...
       span.shortcut {{ controlKey }}+E
-    b-list-group-item.none-border(@click='doAction("editData")', v-show='!isMultipleCellSelected')
+    b-list-group-item.none-border(@click='doAction("editData")', v-show='isEnabled("editData")')
       span.item-name Edit Data...
       span.shortcut {{ controlKey }}+M
-    b-list-group-item.none-border(@click='doAction("editLink")', v-show='!isMultipleCellSelected')
+    b-list-group-item.none-border(@click='doAction("editLink")', v-show='isEnabled("editLink")')
       span.item-name Edit Link...
       span.shortcut Alt +Shift+L
     hr.popup-dropdown-divider(
       role='separator',
       aria-orientation='horizontal',
-      v-show='imageSelected || editorUi.actions.get("group").isEnabled() || editorUi.actions.get("ungroup").isEnabled()'
+      v-show='isEnabled("editImage")'
     )
-    b-list-group-item.none-border(@click='editImage', v-show='imageSelected')
+    b-list-group-item.none-border(@click='editImage', v-show='isEnabled("editImage")')
       span.item-name Edit Image...
     hr.popup-dropdown-divider(
       role='separator',
       aria-orientation='horizontal',
-      v-show='imageSelected && (editorUi.actions.get("group").isEnabled() || editorUi.actions.get("ungroup").isEnabled())'
+      v-show='isEnabled("selectVertices")'
     )
-    b-list-group-item.none-border(@click='doAction("group")', v-show='groupEnabled')
-      span.item-name Group
-      span.shortcut {{ controlKey }}+G
-    b-list-group-item(@click='doAction("ungroup")', v-show='ungroupEnabled')
-      span.item-name Ungroup
-      span.shortcut {{ controlKey }}+Shift+G
+    b-list-group-item.none-border(
+      @click='doAction("selectVertices")',
+      v-show='isEnabled("selectVertices")'
+    )
+      span.item-name Select Vertices
+      span.shortcut {{ controlKey }}+Shift+I
+    b-list-group-item.none-border(
+      @click='doAction("selectEdges")',
+      v-show='isEnabled("selectEdges")'
+    )
+      span.item-name Select Edges
+      span.shortcut {{ controlKey }}+Shift+E
+    b-list-group-item.none-border(@click='doAction("selectAll")', v-show='isEnabled("selectAll")')
+      span.item-name Select All
+      span.shortcut {{ controlKey }}{{ !mxClient.IS_MAC ? "+" : "" }}A
 </template>
